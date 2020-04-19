aaba6fa93a044805070ecddcbea0f31c4e7079ce
vietvite <vanviet8367@gmail.com>
2020-04-20 00:49:26 +0700

    Fix dropdown click outside & Style it icons

--- src/components/CombineComponents/AccountButton/AccountButton.js
+++ src/components/CombineComponents/AccountButton/AccountButton.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import {ChevronDown } from 'react-feather'
+import { ChevronDown, FileText, Heart, Box, LogOut } from 'react-feather'
 import ButtonTransparent from '../../BaseComponents/ButtonTransparent/ButtonTransparent'
 import styles from './AccountButton.module.scss'
 import DropdownMenu from '../../BaseComponents/DropdownMenu/DropdownMenu'
+import OutsideAlerter from '../../BaseComponents/OutsideAlerter/OutsideAlerter'
 
-// NOTE: Get from redux
+const orderListIcon = <span className={styles.orderListIcon}><FileText color='white' strokeWidth='1px' size='1.2rem' /></span>
+const favListIcon = <span className={styles.favListIcon}><Heart color='white' strokeWidth='1px' size='1.2rem' /></span>
+const orderLaterIcon = <span className={styles.orderLaterIcon}><Box color='white' strokeWidth='1px' size='1.2rem' /></span>
+const logOutIcon = <span className={styles.logOutIcon}><LogOut color='white' strokeWidth='1px' size='1.2rem' /></span>
+
 const listMenu = [
-  { name: 'Đơn hàng', url: '/cart' },
-  { name: 'Danh sách yêu thích', url: '/favorites' },
-  { name: 'Đăng xuất', url: '/logout' },
+  { icon: orderListIcon, name: 'Đơn hàng', url: '/bills', },
+  { icon: favListIcon, name: 'Danh sách yêu thích', url: '/favorites' },
+  { icon: orderLaterIcon, name: 'Danh sách mua sau', url: '/orderlater' },
+  { icon: logOutIcon, name: 'Đăng xuất', url: '/logout' },
 ]
 
 export default class AccountButton extends React.Component {
@@ -18,6 +24,7 @@ export default class AccountButton extends React.Component {
       dropdownOpen: false
     }
     this.toggleDropdown = this.toggleDropdown.bind(this)
+    this.delayToggle = this.delayToggle.bind(this)
   }
 
   toggleDropdown() {
@@ -25,10 +32,21 @@ export default class AccountButton extends React.Component {
       dropdownOpen: !this.state.dropdownOpen
     })
   }
+
+  /**
+   * Delay dropdown closing, after click route be executed.
+   * Use this because dropdown menu is using OutsideAlerter 
+   * which is immediately execute callback when click inside it.
+   */
+  delayToggle() {
+    setTimeout(() => {
+      this.toggleDropdown()
+    }, 150);
+  }
   render() {
     const { username = 'Username' } = this.props
     return (
-      <div>
+      <div className={styles.account}>
         <div onMouseDown={this.toggleDropdown} >
           <ButtonTransparent>
             <span className={styles.buttonText}>{username}</span>
@@ -37,9 +55,11 @@ export default class AccountButton extends React.Component {
         </div>
 
         {this.state.dropdownOpen && (
-          <div className={styles.dropdownWrapper}>
-            <DropdownMenu listMenu={listMenu} />
-          </div>
+          <OutsideAlerter clickOutsideCallback={this.toggleDropdown} clickInsideCallback={this.delayToggle} >
+            <div className={styles.dropdownWrapper}>
+              <DropdownMenu listMenu={listMenu} />
+            </div>
+          </OutsideAlerter>
         )}
       </div>
     )
