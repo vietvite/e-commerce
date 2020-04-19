344a32deb514d89a0ee562a8aaa54fb5e0ad4288
NHD98 <ndreece1998@gmail.com>
2020-04-19 10:52:18 +0700

    remove location filter from UI, convert filterbar to form

--- src/components/BaseComponents/ColorButton/ColorButton.js
+++ src/components/BaseComponents/ColorButton/ColorButton.js
@@ -3,7 +3,11 @@ import style from "./ColorButton.module.scss";
 
 class ColorButton extends React.Component {
   render() {
-    return <button className={style.colorButton}>{this.props.children}</button>;
+    return (
+      <button type="submit" className={style.colorButton}>
+        {this.props.children}
+      </button>
+    );
   }
 }
 export default ColorButton;
--- src/components/BaseComponents/PriceInput/PriceInput.js
+++ src/components/BaseComponents/PriceInput/PriceInput.js
@@ -2,12 +2,30 @@ import React from "react";
 import style from "./PriceInput.module.scss";
 
 class PriceInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+    };
+  }
+
+  handlePriceChange = (event) => {
+    let value = parseInt(event.target.value);
+    if (value >= 0) {
+      this.setState({ value: value });
+    } else {
+      this.setState({ value: "" });
+    }
+  };
+
   render() {
     return (
       <input
         className={style.priceInput}
         type="number"
         placeholder={this.props.innerText}
+        value={this.state.value}
+        onChange={this.handlePriceChange}
       />
     );
   }
