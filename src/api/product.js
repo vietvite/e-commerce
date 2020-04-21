48205f5d997595f4c340cd22ce295a59f129b697
NHD98 <ndreece1998@gmail.com>
2020-04-21 10:21:46 +0700

    add HomeProduct Component and fetch data

--- src/api/product.js
+++ src/api/product.js
@@ -1,8 +1,9 @@
-import http from './http'
+import http from "./http";
 
 const ProductService = {
-  findById: id => http().get(`/product/${id}`),
-  findByTitle: title => http().get(`/product?search=${title}`)
-}
+  findById: (id) => http().get(`/product/${id}`),
+  findByTitle: (title) => http().get(`/product?search=${title}`),
+  getHomeProductSection: () => http().get("/product/homeproduct"),
+};
 
-export default ProductService
+export default ProductService;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ProductSession from "../components/CombineComponents/ProductSection/ProductSection";
 import Banner from "../components/CombineComponents/Banner/Banner";
+import HomeProduct from "../components/CombineComponents/HomeProduct/HomeProduct";
 function Home() {
   return (
     <div>
       <Banner />
-      <ProductSession />
+      {/* <ProductSession /> */}
+      <HomeProduct />
     </div>
   );
 }
